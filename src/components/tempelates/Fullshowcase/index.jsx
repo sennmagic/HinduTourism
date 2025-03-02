4ea28131afebfa69370a1435492f781fc519ec8d
senmagic <sangamdevkota02@gmail.com>
2025-03-03 01:00:47 +0545

    changes

--- src/components/tempelates/Fullshowcase/index.jsx
+++ src/components/tempelates/Fullshowcase/index.jsx
@@ -5,32 +5,21 @@ import { FaMapMarkerAlt, FaInstagram } from "react-icons/fa";
 import Marquee from "react-fast-marquee";
 import TextHeader from "@/components/atoms/TextHeadings/Header";
 
-const Showcase = ({infoheader , textheader}) => {
-
-
+const Showcase = ({ infoheader, textheader }) => {
   const smallImages = [
     { src: "/images/Insta1.jpg", alt: "Small Image 1" },
     { src: "/images/Insta2.jpg", alt: "Small Image 2" },
     { src: "/images/Insta3.jpg", alt: "Small Image 3" },
-    
     { src: "/images/Insta4.jpg", alt: "Small Image 5" },
     { src: "/images/Insta1.jpg", alt: "Small Image 8" },
     { src: "/images/Insta2.jpg", alt: "Small Image 9" },
     { src: "/images/Insta3.jpg", alt: "Small Image 10" },
-   
     { src: "/images/Insta4.jpg", alt: "Small Image 12" },
-  
     { src: "/images/Insta1.jpg", alt: "Small Image 8" },
     { src: "/images/Insta2.jpg", alt: "Small Image 9" },
     { src: "/images/Insta3.jpg", alt: "Small Image 10" },
-   
     { src: "/images/Insta4.jpg", alt: "Small Image 12" },
-  
-    
-   
-    
   ];
-  
 
   const [isMarqueePaused, setIsMarqueePaused] = useState(false);
   const [marqueeSpeed, setMarqueeSpeed] = useState(0); // Default speed
@@ -47,7 +36,7 @@ const Showcase = ({infoheader , textheader}) => {
         setMarqueeSpeed(30);
       } else {
         // Larger devices
-        setMarqueeSpeed(0);
+        setMarqueeSpeed(10);
       }
     };
 
@@ -63,70 +52,69 @@ const Showcase = ({infoheader , textheader}) => {
 
   return (
     <>
-{infoheader && textheader ? (
-  <div className="mb-12 max-w-7xl mx-auto my-spacer px-6">
-    <div className="text-[#ac3c16] text-xs font-normal font-['Inter'] leading-[18px] tracking-tight mb-2">
-      {infoheader}
-    </div>
-    <TextHeader
-      text={textheader}
-      size="small"
-      specialWordsIndices="2"
-      align="start"
-      className=""
-    />
-  </div>
-) : null}
-
-    <div className="max-w-full mx-auto overflow-hidden">
-      <div>
-        <Marquee
-          speed={marqueeSpeed}
-          gradient={false}
-          pauseOnHover
-          play={!isMarqueePaused}
-          onTouchStart={handleTouchStart}
-          onTouchEnd={handleTouchEnd}
-        >
-          <div className="flex">
-            {smallImages.map((image, index) => (
-              <div
-                key={index}
-                className="relative group aspect-[3/4] overflow-hidden !rounded-none"
-              >
-                {/* Image */}
-                <Image
-                  src={image.src}
-                  alt={image.alt}
-                  width={200}
-                  height={200}
-                  className="object-cover !border-none !rounded-none"
-                />
-
-                {/* Instagram Logo (static) */}
-                <div className="absolute top-4 left-4 z-10 text-white text-lg">
-                  <FaInstagram className="mr-2 text-white" />
-                </div>
-
-                {/* Overlay Content */}
-                <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-start items-start p-4 opacity-0 group-hover:opacity-60 transition-opacity duration-300">
-                  <div className="flex items-center gap-2 mb-2">
-                    <p className="text-white text-sm ml-6 font-bold">TheHinduTourism</p>
-                  </div>
-                  <div className="flex items-center gap-2">
-                    <FaMapMarkerAlt className="text-white" />
-                    <p className="text-white text-sm">Nepal</p>
-                  </div>
-                </div>
-              </div>
-            ))}
+      {infoheader && textheader ? (
+        <div className="mb-12 max-w-7xl mx-auto my-spacer px-6">
+          <div className="text-[#ac3c16] text-xs font-normal font-['Inter'] leading-[18px] tracking-tight mb-2">
+            {infoheader}
           </div>
-        </Marquee>
+          <TextHeader
+            text={textheader}
+            size="small"
+            specialWordsIndices="2"
+            align="start"
+            className=""
+          />
+        </div>
+      ) : null}
+
+      <div className="max-w-full mx-auto overflow-hidden">
+        <div>
+          <Marquee
+            speed={marqueeSpeed}
+            gradient={false}
+            pauseOnHover
+            play={!isMarqueePaused}
+            onTouchStart={handleTouchStart}
+            onTouchEnd={handleTouchEnd}
+          >
+            <div className="flex">
+              {smallImages.map((image, index) => (
+                <div
+                  key={index}
+                  className="relative group aspect-[4/4] overflow-hidden !rounded-none"
+                >
+                  {/* Image */}
+                  <Image
+                    src={image.src}
+                    alt={image.alt}
+                    width={200}
+                    height={499}
+                    className="object-cover !border-none !rounded-none transition-transform duration-300 ease-in-out transform group-hover:scale-110"
+                  />
+
+                  {/* Instagram Logo (static) */}
+                  <div className="absolute top-4 left-4 z-10 text-white text-lg">
+                    <FaInstagram className="mr-2 text-white" />
+                  </div>
+
+                  {/* Overlay Content */}
+                  <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-start items-start p-4 opacity-0 group-hover:opacity-60 transition-opacity duration-300">
+                    <div className="flex items-center gap-2 mb-2">
+                      <p className="text-white text-sm ml-6 font-bold">TheHinduTourism</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                      <FaMapMarkerAlt className="text-white" />
+                      <p className="text-white text-sm">Nepal</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </Marquee>
+        </div>
       </div>
-    </div>
     </>
   );
 };
 
 export default Showcase;
-
