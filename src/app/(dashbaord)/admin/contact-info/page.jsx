cbec1d28f7e4d35d86eb72cc03da2106c0dcb5d3
senmagic <sangamdevkota02@gmail.com>
2025-03-03 16:20:50 +0545

    changes

--- src/app/(dashbaord)/admin/contact-info/page.jsx
+++ src/app/(dashbaord)/admin/contact-info/page.jsx
@@ -2,9 +2,11 @@
 import { fetchAPI } from "@/utils/apiService"
 
 export default async function contactinfo() {
+<>
 
-          const response = await fetchAPI("contacts", "GET");
-         console.log("response" ,response)
+
+
+</>
       
 
    
